82e52c99b9152ae513fead797bcf8119c779e7ef
Henrique <ishenriquealbert@gmail.com>
2021-05-06 22:24:33 -0300

    wip

--- src/components/GlobalMenu/index.tsx
+++ src/components/GlobalMenu/index.tsx
@@ -1,19 +1,29 @@
 import { Button, Menu } from 'antd'
+import Router from 'next/router'
 
 import * as S from './styles'
 
-export const GlobalMenu = () => (
-  <S.HeaderWrapper>
-    <S.DropdownWrapper overlay={menu}>
-      <Button>Henrique</Button>
-    </S.DropdownWrapper>
-  </S.HeaderWrapper>
-)
+export const GlobalMenu = () => {
+  return (
+    <S.HeaderWrapper>
+      <S.DropdownWrapper overlay={MenuDrop} placement="bottomRight">
+        <Button type="text" size="large">
+          Henrique
+        </Button>
+      </S.DropdownWrapper>
+    </S.HeaderWrapper>
+  )
+}
 
-const menu = (
-  <Menu>
-    <Menu.Item>Minha conta</Menu.Item>
-    <Menu.Item>Configurações</Menu.Item>
-    <Menu.Item>Sair</Menu.Item>
-  </Menu>
-)
+const MenuDrop = () => {
+  return (
+    <Menu>
+      <Menu.Item onClick={() => Router.push('/conta')}>Minha conta</Menu.Item>
+      <Menu.Item onClick={() => Router.push('/conta/configuracoes')}>
+        Configurações
+      </Menu.Item>
+      <Menu.Divider />
+      <Menu.Item>Sair</Menu.Item>
+    </Menu>
+  )
+}
