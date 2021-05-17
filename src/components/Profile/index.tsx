97e155f7a87101607c93dac6ea02f96d2ad3750a
Henrique <ishenriquealbert@gmail.com>
2021-05-16 22:46:03 -0300

    :sparkles: Add Sign up

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,4 +1,5 @@
 import { Flex, Avatar, Text, Box } from '@chakra-ui/react'
+import { useSession } from 'next-auth/client'
 
 type AvatarProps = {
   info?: boolean
@@ -6,23 +7,32 @@ type AvatarProps = {
   p?: string
 }
 
-export const Profile = ({ info, m, p }: AvatarProps) => (
-  <Flex m={m} p={p} align="center">
-    <Avatar w="46px" h="46px" />
-    {info && (
-      <Box pl="16px">
-        <Text fontSize="14px" lineHeight="21px" color="black" fontWeight="500">
-          Sierra Ferguson
-        </Text>
-        <Text
-          fontSize="12px"
-          fontWeight="500"
-          lineHeight="16px"
-          color="gray.500"
-        >
-          s.ferguson@gmail.com
-        </Text>
-      </Box>
-    )}
-  </Flex>
-)
+export const Profile = ({ info, m, p }: AvatarProps) => {
+  const [session] = useSession()
+  return (
+    <Flex m={m} p={p} align="center">
+      <Avatar w="46px" h="46px" src={session?.user?.image || ''} />
+      {info && (
+        <Box pl="16px" maxW="150px">
+          <Text
+            fontSize="14px"
+            lineHeight="21px"
+            color="black"
+            fontWeight="500"
+          >
+            {session?.user?.name}
+          </Text>
+          <Text
+            fontSize="12px"
+            fontWeight="500"
+            lineHeight="16px"
+            color="gray.500"
+            isTruncated
+          >
+            {session?.user?.email}
+          </Text>
+        </Box>
+      )}
+    </Flex>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import { Button, Center } from '@chakra-ui/react'
-import Link from 'next/link'
+import { signIn } from 'next-auth/client'
 
 export default function HomePage() {
   return (
     <Center bg="gray.300" h="100vh" display="flex" flexDirection="column">
-      <Link href="/dashboard" passHref>
-        <Button mb="1rem">Entrar</Button>
-      </Link>
+      <Button
+        onClick={() =>
+          signIn('google', { callbackUrl: 'http://localhost:3000/dashboard' })
+        }
+      >
+        Sign in with Google
+      </Button>
     </Center>
   )
 }
