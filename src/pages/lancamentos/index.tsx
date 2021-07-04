82cebe75b6a797e30f7a9f968bd4f2ec13638ad0
Henrique <ishenriquealbert@gmail.com>
2021-07-04 13:42:15 -0300

    :building_construction: Update module name

--- src/pages/lancamentos/index.tsx
+++ src/pages/lancamentos/index.tsx
@@ -1,6 +1,6 @@
 import { useAllReleasesQuery } from 'graphql/generated'
 import { PrivateLayout, ErrorMessage } from 'components'
-import { ReleaseListSkeleton, ReleaseList } from 'modules/Releases/components'
+import { ReleaseListSkeleton, ReleaseList } from 'modules/releases/components'
 
 export default function ReleasesPage() {
   const { data, isError, isLoading } = useAllReleasesQuery()
